814c4fcd3e9e8418aeb11ab3325de45b36d0fbe7
catshirtguy <emland2@outlook.com>
2017-12-11 16:17:52 -0700

    Search pattern changed to rectangle

--- src/behaviours/src/SearchController.cpp
+++ src/behaviours/src/SearchController.cpp
@@ -1,19 +1,24 @@
+
 #include "SearchController.h"
 #include <angles/angles.h>
 
+#define MAX_DIST 5.0
+#define MIN_DIST 0.5
+#define ANGLE_STEP 0.1
+
 SearchController::SearchController() {
-  rng = new random_numbers::RandomNumberGenerator();
-  currentLocation.x = 0;
+//  rng = new random_numbers::RandomNumberGenerator();
+  currentLocation.x = 0.0;
   currentLocation.y = 0;
   currentLocation.theta = 0;
 
-  centerLocation.x = 0;
+  centerLocation.x = 0.0;
   centerLocation.y = 0;
   centerLocation.theta = 0;
   result.PIDMode = FAST_PID;
-
   result.fingerAngle = M_PI/2;
   result.wristAngle = M_PI/4;
+
 }
 
 void SearchController::Reset() {
@@ -39,33 +44,33 @@ Result SearchController::DoWork() {
     }
     return result;
   }
-  else if (attemptCount >= 5 || attemptCount == 0) 
+  else if (attemptCount >= 5 || attemptCount == 0)
   {
     attemptCount = 1;
-
 
     result.type = waypoint;
     Point  searchLocation;
 
-    //select new position 50 cm from current location
+    //select new position 5.0 meters from current location
     if (first_waypoint)
     {
       first_waypoint = false;
-      searchLocation.theta = currentLocation.theta + M_PI;
-      searchLocation.x = currentLocation.x + (0.5 * cos(searchLocation.theta));
-      searchLocation.y = currentLocation.y + (0.5 * sin(searchLocation.theta));
+
+      searchLocation.theta = currentLocation.theta + M_PI_2; //90 degrees in radians
+      searchLocation.x = currentLocation.x + (MAX_DIST * cos(searchLocation.theta));
+      searchLocation.y = currentLocation.y + (MAX_DIST * sin(searchLocation.theta));
     }
+
+    //select new direction
     else
     {
-      //select new heading from Gaussian distribution around current heading
-      searchLocation.theta = rng->gaussian(currentLocation.theta, 0.785398); //45 degrees in radians
-      searchLocation.x = currentLocation.x + (0.5 * cos(searchLocation.theta));
-      searchLocation.y = currentLocation.y + (0.5 * sin(searchLocation.theta));
+      searchLocation.theta = currentLocation.theta + M_PI_2; //90 degrees in radians
+      searchLocation.x = currentLocation.x + (MAX_DIST * cos(searchLocation.theta));
+      searchLocation.y = currentLocation.y + (MAX_DIST * sin(searchLocation.theta));
     }
 
     result.wpts.waypoints.clear();
-    result.wpts.waypoints.insert(result.wpts.waypoints.begin(), searchLocation);
-    
+    result.wpts.waypoints.insert(result.wpts.waypoints.begin(),searchLocation);
     return result;
   }
 
