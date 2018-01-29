e12ca0a256404e5263ad807b1b5e705722e6dbb1
Jonathan West <jonathan.west@bie.edu>
2018-01-29 09:42:10 -0700

    major rewrite of abridge underway but not finished

--- src/sipi_controller/src/abridge.cpp
+++ src/sipi_controller/src/abridge.cpp
@@ -17,20 +17,36 @@
 //Package include
 #include "sipi_controller/usbSerial.h"
 
+// maximum +/- limits for velocity commands
+#define MAX_LIN_VEL_CMD 0.3  // m/s
+#define MAX_ANG_VEL_CMD 0.5  // rad/s
+// maximum safe value to write to motor controller
+#define MAX_MOTOR_CMD 120
+
 using namespace std;
 
 //aBridge functions
+// theses are the topics that need to communcate through the arduino
+// give drive commands.  only listens to linear:x (m/s) and angular:z (rad/s)
 void driveCommandHandler(const geometry_msgs::Twist::ConstPtr& message);
+// angle in radians for finger joint
 void fingerAngleHandler(const std_msgs::Float32::ConstPtr& angle);
+// angle in radians for wrist joint
 void wristAngleHandler(const std_msgs::Float32::ConstPtr& angle);
+
+// timer to trigger read of serial data from arduino
 void serialActivityTimer(const ros::TimerEvent& e);
+// helper function to publish all of the ROS topics from arduino data
 void publishRosTopics();
+//parse the serial data from the arduino into data structures
 void parseData(string data);
+
 std::string getHumanFriendlyTime();
 
 //Globals
 geometry_msgs::QuaternionStamped fingerAngle;
 geometry_msgs::QuaternionStamped wristAngle;
+geometry_msgs::Twist cmd_vel_msg;
 sensor_msgs::Imu imu;
 nav_msgs::Odometry odom;
 sensor_msgs::Range sonarLeft;
@@ -45,32 +61,9 @@ const float deltaTime = 0.1; //abridge's update interval
 int currentMode = 0;
 string publishedName;
 
-// Allowing messages to be sent to the arduino too fast causes a disconnect
-// This is the minimum time between messages to the arduino in microseconds.
-// Only used with the gripper commands to fix a manual control bug.
-unsigned int min_usb_send_delay = 100;
-
 float heartbeat_publish_interval = 2;
 
-
-//PID constants and arrays
-const int histArrayLength = 1000;
-
-float velFF = 0; //velocity feed forward
-int stepV = 0; //keeps track of the point in the evArray for adding new error each update cycle.
-float evArray[histArrayLength]; //history array of previous error for (arraySize/hz) seconds (error Velocity Array)
-float velError[4] = {0,0,0,0}; //contains current velocity error and error 3 steps in the past.
-
-int stepY = 0; //keeps track of the point in the eyArray for adding new error each update cycle.
-float eyArray[histArrayLength]; //history array of previous error for (arraySize/hz) seconds (error Yaw Array)
-float yawError[4] = {0,0,0,0}; //contains current yaw error and error 3 steps in the past.
-
-float prevLin = 0;
-float prevYaw = 0;
-
-ros::Time prevDriveCommandUpdateTime;
-
-//Publishers
+//Publishers for ROS data from the Arduino
 ros::Publisher fingerAnglePublish;
 ros::Publisher wristAnglePublish;
 ros::Publisher imuPublish;
@@ -106,106 +99,104 @@ int main(int argc, char **argv) {
   param.param("device", devicePath, string("/dev/ttyUSB0"));
   usb.openUSBPort(devicePath, baud);
 
-
   sleep(5);
 
   ros::NodeHandle aNH;
 
   if (argc >= 2) {
     publishedName = argv[1];
-    cout << "Welcome to the world of tomorrow " << publishedName << "!  ABridge module started." << endl;
+    ROS_INFO_STREAM(publishedName << ": ABridge module started." << endl);
   } else {
     publishedName = hostname;
-    cout << "No Name Selected. Default is: " << publishedName << endl;
+    ROS_INFO_STREAM("abridge: No Name Selected. Default is: " << 
+      publishedName << endl);
   }
 
-  fingerAnglePublish = aNH.advertise<geometry_msgs::QuaternionStamped>((publishedName + "/fingerAngle/prev_cmd"), 10);
-  wristAnglePublish = aNH.advertise<geometry_msgs::QuaternionStamped>((publishedName + "/wristAngle/prev_cmd"), 10);
+  fingerAnglePublish = aNH.advertise<geometry_msgs::QuaternionStamped>(
+    (publishedName + "/fingerAngle/prev_cmd"), 10);
+  wristAnglePublish = aNH.advertise<geometry_msgs::QuaternionStamped>(
+    (publishedName + "/wristAngle/prev_cmd"), 10);
   imuPublish = aNH.advertise<sensor_msgs::Imu>((publishedName + "/imu"), 10);
-  odomPublish = aNH.advertise<nav_msgs::Odometry>((publishedName + "/odom"), 10);
-  sonarLeftPublish = aNH.advertise<sensor_msgs::Range>((publishedName + "/sonarLeft"), 10);
-  sonarCenterPublish = aNH.advertise<sensor_msgs::Range>((publishedName + "/sonarCenter"), 10);
-  sonarRightPublish = aNH.advertise<sensor_msgs::Range>((publishedName + "/sonarRight"), 10);
+  odomPublish = aNH.advertise<nav_msgs::Odometry>(
+    (publishedName + "/odom"), 10);
+  sonarLeftPublish = aNH.advertise<sensor_msgs::Range>(
+    (publishedName + "/sonarLeft"), 10);
+  sonarCenterPublish = aNH.advertise<sensor_msgs::Range>(
+    (publishedName + "/sonarCenter"), 10);
+  sonarRightPublish = aNH.advertise<sensor_msgs::Range>(
+    (publishedName + "/sonarRight"), 10);
   infoLogPublisher = aNH.advertise<std_msgs::String>("/infoLog", 1, true);
-  heartbeatPublisher = aNH.advertise<std_msgs::String>((publishedName + "/abridge/heartbeat"), 1, true);
+  heartbeatPublisher = aNH.advertise<std_msgs::String>(
+    (publishedName + "/abridge/heartbeat"), 1, true);
 
-  driveControlSubscriber = aNH.subscribe((publishedName + "/driveControl"), 10, driveCommandHandler);
-  fingerAngleSubscriber = aNH.subscribe((publishedName + "/fingerAngle/cmd"), 1, fingerAngleHandler);
-  wristAngleSubscriber = aNH.subscribe((publishedName + "/wristAngle/cmd"), 1, wristAngleHandler);
-  modeSubscriber = aNH.subscribe((publishedName + "/mode"), 1, modeHandler);
+  driveControlSubscriber = aNH.subscribe(
+    (publishedName + "/driveControl"), 10, driveCommandHandler);
+  fingerAngleSubscriber = aNH.subscribe(
+    (publishedName + "/fingerAngle/cmd"), 1, fingerAngleHandler);
+  wristAngleSubscriber = aNH.subscribe(
+    (publishedName + "/wristAngle/cmd"), 1, wristAngleHandler);
+  modeSubscriber = aNH.subscribe(
+    (publishedName + "/mode"), 1, modeHandler);
 
 
   publishTimer = aNH.createTimer(ros::Duration(deltaTime), serialActivityTimer);
-  publish_heartbeat_timer = aNH.createTimer(ros::Duration(heartbeat_publish_interval), publishHeartBeatTimerEventHandler);
+  publish_heartbeat_timer = aNH.createTimer(
+    ros::Duration(heartbeat_publish_interval), 
+    publishHeartBeatTimerEventHandler);
 
   imu.header.frame_id = publishedName+"/base_link";
 
   odom.header.frame_id = publishedName+"/odom";
   odom.child_frame_id = publishedName+"/base_link";
 
-  for (int i = 0; i < histArrayLength; i++)
-  {
-    evArray[i] = 0;
-    eyArray[i] = 0;
-  }
-
-  prevDriveCommandUpdateTime = ros::Time::now();
-
   ros::spin();
 
   return EXIT_SUCCESS;
 }
-
-//This command handler recives a linear velocity setpoint and a angular yaw error
-//and produces a command output for the left and right motors of the robot.
-//See the following paper for description of PID controllers.
-//Bennett, Stuart (November 1984). "Nicholas Minorsky and the automatic steering of ships". IEEE Control Systems Magazine. 4 (4): 10–15. doi:10.1109/MCS.1984.1104827. ISSN 0272-1708.
-void driveCommandHandler(const geometry_msgs::Twist::ConstPtr& message) {
-
-
-  float left = (message->linear.x); //target linear velocity in meters per second
-  float right = (message->angular.z); //angular error in radians
-
-  // Cap motor commands at 120. Experimentally determined that high values (tested 180 and 255) can cause 
-  // the hardware to fail when the robot moves itself too violently.
-  int max_motor_cmd = 120;
-
-  // Check that the resulting motor commands do not exceed the specified safe maximum value
-  if (left > max_motor_cmd)
-  {
-    left = max_motor_cmd;
-  }
-  else if (left < -max_motor_cmd)
-  {
-    left = - max_motor_cmd;
-  }
-
-  if (right > max_motor_cmd)
-  {
-    right = max_motor_cmd;
-  }
-  else if (right < -max_motor_cmd)
-  {
-    right = -max_motor_cmd;
-  }
-
-  int leftInt = left;
-  int rightInt = right;
-
-  sprintf(moveCmd, "v,%d,%d\n", leftInt, rightInt); //format data for arduino into c string
-  usb.sendData(moveCmd);                      //send movement command to arduino over usb
-  memset(&moveCmd, '\0', sizeof (moveCmd));   //clear the movement command string
+float limit(float val, float max) {
+  if (val > max) {
+    return max;
+  } else if (val < -max) {
+    return -max;
+  } else {
+    return val;
+  } 
 }
 
+//This command handler recives a twist setpoint
+void driveCommandHandler(const geometry_msgs::Twist::ConstPtr& message) {
+  cmd_vel_msg = *message;
+  cmd_vel_msg.linear.x = limit(cmd_vel_msg.linear.x, MAX_LIN_VEL_CMD); 
+  cmd_vel_msg.angular.z = limit(cmd_vel_msg.angular.z, MAX_LIN_VEL_CMD); 
+}
 
-// The finger and wrist handlers receive gripper angle commands in floating point
+void calculateMotorCommands(void) {
+  // Cap motor commands at 120. Experimentally determined that 
+  // high values (tested 180 and 255) can cause 
+  // the hardware to fail when the robot moves itself too violently.
+
+  // limit motor commands to safe maximum value
+  geometry_msgs::Twist err;
+  err.linear.x = odom.twist.twist.linear.x - cmd_vel_msg.linear.x;
+  err.angular.z = odom.twist.twist.angular.z - cmd_vel_msg.angular.z;
+  float Kp = 10;
+  float Ki = 10;
+  int vx = err.linear.x * Kp;
+  int vz = err.angular.z * Kp;
+  
+  int motor_left = vx - vz;
+  int motor_right = vx + vz;
+
+  //format data for arduino into c string
+  sprintf(moveCmd, "v,%d,%d\n", motor_left, motor_right); 
+  usb.sendData(moveCmd);       //send movement command to arduino over usb
+}
+
+// The finger and wrist handlers receive gripper angle commands in 
+// floating point
 // radians, write them to a string and send that to the arduino
 // for processing.
 void fingerAngleHandler(const std_msgs::Float32::ConstPtr& angle) {
-
-  // To throttle the message rate so we don't lose connection to the arduino
-  usleep(min_usb_send_delay);
-
   char cmd[16]={'\0'};
 
   // Avoid dealing with negative exponents which confuse the conversion to string by checking if the angle is small
@@ -216,12 +207,9 @@ void fingerAngleHandler(const std_msgs::Float32::ConstPtr& angle) {
     sprintf(cmd, "f,%.4g\n", angle->data);
   }
   usb.sendData(cmd);
-  memset(&cmd, '\0', sizeof (cmd));
 }
 
 void wristAngleHandler(const std_msgs::Float32::ConstPtr& angle) {
-  // To throttle the message rate so we don't lose connection to the arduino
-  usleep(min_usb_send_delay);
 
   char cmd[16]={'\0'};
 
@@ -233,13 +221,13 @@ void wristAngleHandler(const std_msgs::Float32::ConstPtr& angle) {
     sprintf(cmd, "w,%.4g\n", angle->data);
   }
   usb.sendData(cmd);
-  memset(&cmd, '\0', sizeof (cmd));
 }
 
 void serialActivityTimer(const ros::TimerEvent& e) {
   usb.sendData(dataCmd);
   parseData(usb.readData());
   publishRosTopics();
+  calculateMotorCommands();
 }
 
 void publishRosTopics() {
@@ -269,11 +257,13 @@ void parseData(string str) {
 
       if (dataSet.at(0) == "GRF") {
         fingerAngle.header.stamp = ros::Time::now();
-        fingerAngle.quaternion = tf::createQuaternionMsgFromRollPitchYaw(atof(dataSet.at(2).c_str()), 0.0, 0.0);
+        fingerAngle.quaternion = tf::createQuaternionMsgFromRollPitchYaw(
+          atof(dataSet.at(2).c_str()), 0.0, 0.0);
       }
       else if (dataSet.at(0) == "GRW") {
         wristAngle.header.stamp = ros::Time::now();
-        wristAngle.quaternion = tf::createQuaternionMsgFromRollPitchYaw(atof(dataSet.at(2).c_str()), 0.0, 0.0);
+        wristAngle.quaternion = tf::createQuaternionMsgFromRollPitchYaw(
+          atof(dataSet.at(2).c_str()), 0.0, 0.0);
       }
       else if (dataSet.at(0) == "IMU") {
         imu.header.stamp = ros::Time::now();
@@ -283,14 +273,17 @@ void parseData(string str) {
         imu.angular_velocity.x = atof(dataSet.at(5).c_str());
         imu.angular_velocity.y = atof(dataSet.at(6).c_str());
         imu.angular_velocity.z = atof(dataSet.at(7).c_str());
-        imu.orientation = tf::createQuaternionMsgFromRollPitchYaw(atof(dataSet.at(8).c_str()), atof(dataSet.at(9).c_str()), atof(dataSet.at(10).c_str()));
+        imu.orientation = tf::createQuaternionMsgFromRollPitchYaw(
+          atof(dataSet.at(8).c_str()), atof(dataSet.at(9).c_str()), 
+          atof(dataSet.at(10).c_str()));
       }
       else if (dataSet.at(0) == "ODOM") {
         odom.header.stamp = ros::Time::now();
         odom.pose.pose.position.x += atof(dataSet.at(2).c_str()) / 100.0;
         odom.pose.pose.position.y += atof(dataSet.at(3).c_str()) / 100.0;
         odom.pose.pose.position.z = 0.0;
-        odom.pose.pose.orientation = tf::createQuaternionMsgFromYaw(atof(dataSet.at(4).c_str()));
+        odom.pose.pose.orientation = tf::createQuaternionMsgFromYaw(
+          atof(dataSet.at(4).c_str()));
         odom.twist.twist.linear.x = atof(dataSet.at(5).c_str()) / 100.0;
         odom.twist.twist.linear.y = atof(dataSet.at(6).c_str()) / 100.0;
         odom.twist.twist.angular.z = atof(dataSet.at(7).c_str());
@@ -311,8 +304,6 @@ void parseData(string str) {
     }
   }
 }
-
-
 
 void modeHandler(const std_msgs::UInt8::ConstPtr& message) {
   currentMode = message->data;
