5b4aeb6927389c87465ae42330199d92456e2fd8
catshirtguy <emland2@outlook.com>
2018-02-07 11:20:10 -0700

    changed rover launch script to point to the one in sipi_controller

--- src/sipi_controller/src/sipi_controller.cpp
+++ src/sipi_controller/src/sipi_controller.cpp
@@ -594,7 +594,7 @@ void joyCmdHandler(const sensor_msgs::Joy::ConstPtr& message) {
 
 void publishStatusTimerEventHandler(const ros::TimerEvent&) {
   std_msgs::String msg;
-  msg.data = "online";
+  msg.data = "online-SIPI";
   status_publisher.publish(msg);
 }
 
