4abc6c0f0f77c62ce97d838628aa62a3ffcd1c12
Matthew Fricke <matthew@fricke.co.uk>
2018-03-19 15:23:57 -0600

    removed some prints and gave abridge name argument

--- src/sipi_controller/src/sipi_controller_node.cpp
+++ src/sipi_controller/src/sipi_controller_node.cpp
@@ -14,11 +14,8 @@ int main(int argc, char **argv) {
     publishedName = hostname;
     cout << "No Name Selected. Default is: " << publishedName << endl;
   }
-  cout << "controller HERE\n";
   ros::init(argc, argv, (publishedName + "_BEHAVIOUR") );
-  cout << "controller HERE1\n";
   sipi_controller controller(publishedName, argc, argv);
-  cout << "controller HERE2\n";
   ros::spin();
 
   return EXIT_SUCCESS;
